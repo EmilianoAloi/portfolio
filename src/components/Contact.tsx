efa9497beb38d2055c6f2e41ac37cfd496582c1e
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-05-09 20:45:10 -0300

    input form email - Done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,28 +7,32 @@ const Contact = () => {
                 <p className='text-md text-zinc-500'>Completá el siguiente formulario y te responderé dentro de las próximas 24 horas.</p>
             </div>
 
-            <form action="" className="grid grid-flow-row-dense grid-cols-3 grid-rows-5 gap-2">
+            <form action="" className="grid grid-flow-row-dense grid-cols-5 grid-rows-5 gap-20">
 
-                <div className="flex flex-col gap-1 w-75">
+                {/* Name */}
+                <div className="flex flex-col col-span-2 gap-1">
                     <p className="flex items-center justify-center text-zinc-500 w-5 h-5 text-xs font-bold border-solid border-2 border-zinc-500 rounded-full ">
                         1
                     </p>
                     <label htmlFor="name" className="font-semibold text-xl">Como te llamas?</label>
                     <p className="label-required font-semibold text-xs">REQUIRED</p>
                 </div>
-
-
-                <div className="flex items-center col-span-2">
-                    <input type="text" id="name" name="name" className="w-full" />
+                <div className="flex items-end col-span-3 ">
+                    <input type="text" id="name" className="w-full px-6" placeholder="Escribe tu nombre completo" />
                 </div>
 
+                {/* Email */}
+                <div className="flex flex-col col-span-2 gap-1">
+                    <p className="flex items-center justify-center text-zinc-500 w-5 h-5 text-xs font-bold border-solid border-2 border-zinc-500 rounded-full ">
+                        2
+                    </p>
+                    <label htmlFor="name" className="font-semibold text-xl">Cual es tu correo electrónico?</label>
+                    <p className="label-required font-semibold text-xs">REQUIRED</p>
+                </div>
+                <div className="flex items-end col-span-3 ">
+                    <input type="text" id="name" className="w-full px-6" placeholder="ejemplo@gmail.com" />
+                </div>
 
-
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" />
-
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
 
 
                 <div>
