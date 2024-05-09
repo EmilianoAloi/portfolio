24d6417fd6bc1f42860ea90729abb47cc5a02add
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-05-09 11:45:51 -0300

    Contact section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Experience />
       <Skills />
       <Projects />
+      <Contact />
     </main>
 
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+
+const Contact = () => {
+    return (
+        <section className="w-full max-w-screen-lg mx-auto flex flex-col my-28">
+            <h2 className='text-5xl font-bold'>Contacto</h2>
+            <p className='text-md text-zinc-500'>Completá el siguiente formulario y te responderé dentro de las próximas 24 horas.</p>
+
+
+        </section>
+    )
+}
+
+export default Contact
