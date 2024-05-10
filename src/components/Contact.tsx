70f7beaa45ce6f7ddbe2763ee0ba3335f31a1e85
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-05-09 21:30:55 -0300

    input form Textarea - Done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
                 <p className='text-md text-zinc-500'>Completá el siguiente formulario y te responderé dentro de las próximas 24 horas.</p>
             </div>
 
-            <form action="" className="grid grid-flow-row-dense grid-cols-5 grid-rows-5 gap-20">
+            <form action="" className="grid grid-flow-row-dense grid-cols-5 grid-rows-10 gap-20">
 
                 {/* Name */}
                 <div className="flex flex-col col-span-2 gap-1">
@@ -26,14 +26,37 @@ const Contact = () => {
                     <p className="flex items-center justify-center text-zinc-500 w-5 h-5 text-xs font-bold border-solid border-2 border-zinc-500 rounded-full ">
                         2
                     </p>
-                    <label htmlFor="name" className="font-semibold text-xl">Cual es tu correo electrónico?</label>
+                    <label htmlFor="email" className="font-semibold text-xl">Cual es tu correo electrónico?</label>
                     <p className="label-required font-semibold text-xs">REQUIRED</p>
                 </div>
                 <div className="flex items-end col-span-3 ">
-                    <input type="text" id="name" className="w-full px-6" placeholder="ejemplo@gmail.com" />
+                    <input type="text" id="email" className="w-full px-6" placeholder="ejemplo@gmail.com" />
+                </div>
+
+                {/* TextArea */}
+                <div className="flex flex-col col-span-2 row-span-4 gap-1">
+                    <p className="flex items-center justify-center text-zinc-500 w-5 h-5 text-xs font-bold border-solid border-2 border-zinc-500 rounded-full ">
+                        4
+                    </p>
+                    <label htmlFor="message" className="font-semibold text-xl">¿Me contas sobre tu proyecto / producto / empresa en pocas palabras?</label>
+                    <p className="label-required font-semibold text-xs">REQUIRED</p>
+                </div>
+                <div className="flex items-start pt-8 col-span-3 row-span-4 ">
+                    <textarea id="message" rows={10} className="w-full p-4" placeholder=" Escribe tu consulta" />
                 </div>
 
 
+                {/* Servicios */}
+                {/* <div className="flex flex-col col-span-2 gap-1">
+                    <p className="flex items-center justify-center text-zinc-500 w-5 h-5 text-xs font-bold border-solid border-2 border-zinc-500 rounded-full ">
+                        2
+                    </p>
+                    <label htmlFor="service1" className="font-semibold text-xl">¿Qué tipo de servicio estás buscando?</label>
+                    <p className="label-required font-semibold text-xs">REQUIRED</p>
+                </div>
+                <div className="flex items-end  col-span-3 ">
+                    <input type="checkbox" id="service1" className="w-full px-6" />
+                </div> */}
 
                 <div>
 
