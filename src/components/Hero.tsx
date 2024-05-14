b147d0e515e5ca6e1474746b7a2d92d1ddfe475d
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-05-14 19:36:08 -0300

    framer motion installed, available in navbar: animated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero: FC = () => {
     return (
         <>
             <div><CoverParticles /></div>
-            <section className="w-full lg:max-w-screen-lg mx-auto grid grid-cols-1 grid-rows-1  items-center h-full bgDark ">
+            <section className="w-full h-screen lg:max-w-screen-lg mx-auto grid grid-cols-1 grid-rows-1  items-center h-full bgDark ">
                 <div className="container text-center mx-auto flex flex-col lg:gap-3 justify-end backdrop-blur-4xl backdrop-filter bgDark w-fit">
                     {/* <p className="text-5xl lg:text-8xl">🚀</p> */}
 
@@ -28,7 +28,7 @@ const Hero: FC = () => {
                     <h3 className="text-2xl lg:text-6xl text-zinc-700 bgDark z-10 pb-2">Desarrollador de Software</h3>
                     <h3 className="text-zinc-400 lg:text-3xl bgDark z-10">
                         +2 años de experiencia creando aplicaciones web <br />
-                        Desde Buenos Aires hacia el Mundo, <br />
+                        desde Buenos Aires hacia el Mundo, <br />
                         atractivas y fáciles de usar.
                     </h3>
                     <div className="flex items-end gap-10 hidden">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { MuseoModerno } from "next/font/google"
 import Hamburger from 'hamburger-react'
 import githubLogo from "../../public/socialLogos/github.png"
 import linkedinLogo from "../../public/socialLogos/ln.png"
-
+import { motion } from "framer-motion"
 
 const museoModerno = MuseoModerno({
     subsets: ["latin"],
@@ -28,22 +28,27 @@ const Navbar: FC = () => {
     return (
         <header className="fixed w-full px-4 py-6 lg:px-10 md:shadow-md z-20 flex justify-center flex-wrap shadow-lg  transition duration-500 ease-in-out backdrop-filter backdrop-blur-md ">
             <nav className=" flex w-full justify-between items-center ">
-                <Link href="/" className="flex gap-5 w-48">
+                <Link href="/" className="flex gap-5 w-52">
                     <div className="ps-2">
                         <h2 className={`font-semibold ${museoModerno.className}`}>Emiliano Aloi</h2>
                         <h3 className="text-zinc-500 text-xs">Desarrollador de Software</h3>
                     </div>
                 </Link>
                 <div className="hidden lg:flex  ">
-                    <ul className="flex  gap-6  font-semibold ">
+                    <ul className="flex gap-6 font-semibold ">
                         <li><Link href="#experience" className="hover:text-zinc-500">Experiencia</Link></li>
                         <li><Link href="#projects" className="hover:text-zinc-500">Proyectos</Link></li>
                         <li><Link href="#skills" className="hover:text-zinc-500">Habilidades</Link></li>
                         <li><Link href="#contact" className="hover:text-zinc-500">Contacto</Link></li>
                     </ul>
                 </div>
-                <ul className="flex gap-4 justify-end items-center hidden lg:flex">
-                    <li className="text-green-400 pe-4">Disponible</li>
+                <ul className="flex gap-4 justify-end items-center hidden lg:flex w-52">
+                    <motion.li className="green-dot"
+                        animate={{ opacity: [0.2, 1, 0.2] }}
+                        transition={{ duration: 2, repeat: Infinity }}
+                    >
+                    </motion.li>
+                    <li className="text-green-400 pe-4 ">Disponible</li>
                     <li className="hidden lg:flex">
                         <Link href="https://github.com/EmilianoAloi" target="_blank" rel="noopener noreferrer">
                             <Image src={githubLogo} width={40} alt="GitHub Logo" />
