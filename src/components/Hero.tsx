4a5bac5c7d80ff103da99471baf73f47505fe022
Emiliano <emilianoaloi.dev@gmail.com>
2024-11-21 10:26:57 -0300

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,55 +1,64 @@
-import CoverParticles from "./CoverParticles"
-import { MuseoModerno } from "next/font/google"
-import { FC } from "react"
-import Link from "next/link"
-import ScrollDownButton from "./ScrollDownButton"
-import { motion } from "framer-motion"
+import CoverParticles from "./CoverParticles";
+import { MuseoModerno } from "next/font/google";
+import { FC } from "react";
+import Link from "next/link";
+import ScrollDownButton from "./ScrollDownButton";
+import { motion } from "framer-motion";
 
 interface TitleFont {
-    subsets: string[];
-    display: string
+  subsets: string[];
+  display: string;
 }
 
 const museoModerno = MuseoModerno({
-    subsets: ["latin"],
-    display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const Hero: FC = () => {
+  return (
+    <>
+      <div>
+        <CoverParticles />
+      </div>
+      <motion.section
+        whileInView={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 1, delay: 1 }}
+        className="w-full h-screen lg:max-w-screen-lg mx-auto grid grid-cols-1 grid-rows-1  items-center h-full bgDark "
+      >
+        <div className="container text-center px-2 mx-auto flex flex-col lg:gap-3 justify-end backdrop-blur-4xl backdrop-filter bgDark w-fit">
+          {/* <p className="text-5xl lg:text-8xl">🚀</p> */}
 
+          <h2
+            className={`text-5xl lg:text-8xl font-semibold bgDark z-10 mx-auto mt-32 ${museoModerno.className}`}
+          >
+            Emiliano Aloi{" "}
+          </h2>
+          <h3 className="text-2xl lg:text-6xl text-zinc-700 bgDark z-10 pb-2">
+            Desarrollador Web
+          </h3>
+          <h3 className="text-zinc-400 lg:text-3xl bgDark z-10">
+            +2 años de experiencia creando <br /> aplicaciones atractivas y
+            fáciles de usar.
+          </h3>
+          <ScrollDownButton />
+          <div className="flex items-end gap-10 hidden">
+            <Link href={"/"} className="text-md lg:text-xl py-3 ">
+              VER MAS
+            </Link>
+            <Link
+              href="/CVEmilianoAloiFullstack.pdf"
+              download="CVEmilianoAloiFullstack"
+              className="text-md lg:text-xl py-3"
+            >
+              CV
+            </Link>
+          </div>
+        </div>
+      </motion.section>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div><CoverParticles /></div>
-            <motion.section
-                whileInView={{ opacity: 1 }}
-                initial={{ opacity: 0 }}
-                transition={{ duration: 1, delay: 1 }}
-                className="w-full h-screen lg:max-w-screen-lg mx-auto grid grid-cols-1 grid-rows-1  items-center h-full bgDark ">
-                <div className="container text-center px-2 mx-auto flex flex-col lg:gap-3 justify-end backdrop-blur-4xl backdrop-filter bgDark w-fit">
-                    {/* <p className="text-5xl lg:text-8xl">🚀</p> */}
-
-                    <h2 className={`text-5xl lg:text-8xl font-semibold bgDark z-10 mx-auto mt-32 ${museoModerno.className}`}>Emiliano Aloi </h2>
-                    <h3 className="text-2xl lg:text-6xl text-zinc-700 bgDark z-10 pb-2">Desarrollador de Software</h3>
-                    <h3 className="text-zinc-400 lg:text-3xl bgDark z-10">
-                        +2 años de experiencia creando <br /> aplicaciones
-                        atractivas y fáciles de usar.
-                    </h3>
-                    <ScrollDownButton />
-                    <div className="flex items-end gap-10 hidden">
-                        <Link href={"/"} className="text-md lg:text-xl py-3 ">VER MAS</Link>
-                        <Link
-                            href="/CVEmilianoAloiFullstack.pdf"
-                            download="CVEmilianoAloiFullstack"
-                            className="text-md lg:text-xl py-3">
-                            CV
-                        </Link>
-                    </div>
-                </div>
-
-            </motion.section>
-        </>
-    )
-}
-
-export default Hero
+export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,105 +1,166 @@
-"use client"
+"use client";
 
-import { FC, useState } from "react"
-import Link from "next/link"
-import Image from "next/image"
-import { MuseoModerno } from "next/font/google"
-import Hamburger from 'hamburger-react'
-import githubLogo from "../../public/socialLogos/github.png"
-import linkedinLogo from "../../public/socialLogos/ln.png"
-import { motion } from "framer-motion"
+import { FC, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { MuseoModerno } from "next/font/google";
+import Hamburger from "hamburger-react";
+import githubLogo from "../../public/socialLogos/github.png";
+import linkedinLogo from "../../public/socialLogos/ln.png";
+import { motion } from "framer-motion";
 
 const museoModerno = MuseoModerno({
-    subsets: ["latin"],
-    display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
-
-
 const Navbar: FC = () => {
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
 
-    const closeNavbar = () => {
-        setIsOpen(false);
-    }
+  const [isOpen, setIsOpen] = useState(false);
 
+  return (
+    <>
+      <header className="fixed w-full px-4 py-6 lg:px-10 md:shadow-md z-20 flex justify-center flex-wrap shadow-lg  transition duration-500 ease-in-out backdrop-filter backdrop-blur-md ">
+        <nav className=" flex w-full justify-between items-center ">
+          <Link href="/" className="flex gap-5 w-60">
+            <div className="ps-2">
+              <h2 className={`font-semibold ${museoModerno.className}`}>
+                Emiliano Aloi
+              </h2>
+              <h3 className="text-zinc-500 text-xs">Desarrollador Web</h3>
+            </div>
+          </Link>
+          <div className="hidden lg:flex  ">
+            <ul className="flex gap-6 font-semibold ">
+              <li>
+                <Link href="#experience" className="hover:text-zinc-500">
+                  Experiencia
+                </Link>
+              </li>
+              <li>
+                <Link href="#projects" className="hover:text-zinc-500">
+                  Proyectos
+                </Link>
+              </li>
+              <li>
+                <Link href="#skills" className="hover:text-zinc-500">
+                  Habilidades
+                </Link>
+              </li>
+              <li>
+                <Link href="#contact" className="hover:text-zinc-500">
+                  Contacto
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <ul className="flex gap-4 justify-end items-center hidden lg:flex w-60">
+            <motion.li
+              className="green-dot"
+              animate={{ opacity: [0.2, 1, 0.2] }}
+              transition={{ duration: 2, repeat: Infinity }}
+            ></motion.li>
+            <li className="text-green-400 pe-4 ">Disponible</li>
+            <li className="hidden lg:flex">
+              <Link
+                href="https://github.com/EmilianoAloi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image src={githubLogo} width={40} alt="GitHub Logo" />
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                href="https://www.linkedin.com/in/emilianoaloi/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image src={linkedinLogo} width={40} alt="Linkedin Logo" />
+              </Link>
+            </li>
+          </ul>
+          <div className="lg:hidden">
+            <Hamburger toggled={isOpen} toggle={setIsOpen} />
+          </div>
+        </nav>
 
-    const [isOpen, setIsOpen] = useState(false);
+        {isOpen ? (
+          <ul className="lg:hidden flex flex-col gap-8 font-semibold text-center pt-6">
+            <li>
+              <Link
+                href="/"
+                className="hover:text-zinc-500"
+                onClick={closeNavbar}
+              >
+                Home
+              </Link>
+            </li>
 
+            <li>
+              <Link
+                href="#experience"
+                className="hover:text-zinc-500"
+                onClick={closeNavbar}
+              >
+                Experiencia
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#projects"
+                className="hover:text-zinc-500"
+                onClick={closeNavbar}
+              >
+                Proyectos
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#skills"
+                className="hover:text-zinc-500"
+                onClick={closeNavbar}
+              >
+                Habilidades
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#contact"
+                className="hover:text-zinc-500"
+                onClick={closeNavbar}
+              >
+                Contacto
+              </Link>
+            </li>
+            <li>
+              <div className="flex justify-center gap-2">
+                <Link
+                  href="https://github.com/EmilianoAloi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={githubLogo} width={20} alt="GitHub Logo" />
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/in/emilianoaloi/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={linkedinLogo} width={20} alt="Linkedin Logo" />
+                </Link>
+              </div>
+            </li>
+          </ul>
+        ) : (
+          ""
+        )}
+      </header>
+    </>
+  );
+};
 
-
-    return (
-        <>
-
-            <header
-                className="fixed w-full px-4 py-6 lg:px-10 md:shadow-md z-20 flex justify-center flex-wrap shadow-lg  transition duration-500 ease-in-out backdrop-filter backdrop-blur-md "
-            >
-
-                <nav className=" flex w-full justify-between items-center ">
-                    <Link href="/" className="flex gap-5 w-60">
-                        <div className="ps-2">
-                            <h2 className={`font-semibold ${museoModerno.className}`}>Emiliano Aloi</h2>
-                            <h3 className="text-zinc-500 text-xs">Desarrollador de Software</h3>
-                        </div>
-                    </Link>
-                    <div className="hidden lg:flex  ">
-                        <ul className="flex gap-6 font-semibold ">
-                            <li><Link href="#experience" className="hover:text-zinc-500">Experiencia</Link></li>
-                            <li><Link href="#projects" className="hover:text-zinc-500">Proyectos</Link></li>
-                            <li><Link href="#skills" className="hover:text-zinc-500">Habilidades</Link></li>
-                            <li><Link href="#contact" className="hover:text-zinc-500">Contacto</Link></li>
-                        </ul>
-                    </div>
-                    <ul className="flex gap-4 justify-end items-center hidden lg:flex w-60">
-                        <motion.li className="green-dot"
-                            animate={{ opacity: [0.2, 1, 0.2] }}
-                            transition={{ duration: 2, repeat: Infinity }}
-                        >
-                        </motion.li>
-                        <li className="text-green-400 pe-4 ">Disponible</li>
-                        <li className="hidden lg:flex">
-                            <Link href="https://github.com/EmilianoAloi" target="_blank" rel="noopener noreferrer">
-                                <Image src={githubLogo} width={40} alt="GitHub Logo" />
-                            </Link>
-                        </li>
-                        <li className="">
-                            <Link href="https://www.linkedin.com/in/emilianoaloi/" target="_blank" rel="noopener noreferrer">
-                                <Image src={linkedinLogo} width={40} alt="Linkedin Logo" />
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className="lg:hidden">
-                        <Hamburger toggled={isOpen} toggle={setIsOpen} />
-                    </div>
-                </nav>
-
-                {isOpen ?
-                    <ul
-                        className="lg:hidden flex flex-col gap-8 font-semibold text-center pt-6"                    >
-                        <li><Link href="/" className="hover:text-zinc-500" onClick={closeNavbar}>Home</Link></li>
-
-                        <li><Link href="#experience" className="hover:text-zinc-500" onClick={closeNavbar}>Experiencia</Link></li>
-                        <li><Link href="#projects" className="hover:text-zinc-500" onClick={closeNavbar}>Proyectos</Link></li>
-                        <li><Link href="#skills" className="hover:text-zinc-500" onClick={closeNavbar}>Habilidades</Link></li>
-                        <li><Link href="#contact" className="hover:text-zinc-500" onClick={closeNavbar}>Contacto</Link></li>
-                        <li>
-                            <div className="flex justify-center gap-2">
-                                <Link href="https://github.com/EmilianoAloi" target="_blank" rel="noopener noreferrer">
-                                    <Image src={githubLogo} width={20} alt="GitHub Logo" />
-                                </Link>
-                                <Link href="https://www.linkedin.com/in/emilianoaloi/" target="_blank" rel="noopener noreferrer">
-                                    <Image src={linkedinLogo} width={20} alt="Linkedin Logo" />
-                                </Link>
-                            </div>
-                        </li>
-
-                    </ul>
-                    : ""
-                }
-            </header>
-
-        </>
-
-    )
-}
-
-export default Navbar
+export default Navbar;
